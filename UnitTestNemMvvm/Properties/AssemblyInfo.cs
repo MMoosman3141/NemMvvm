79ec2c3036db91b39a17e55ca91fe106733c04a1
MMoosman3141 <mark.moosman@gmail.com>
2016-11-03 20:40:50 -0600

    Updated handling for Nuget packaging.  Added unit test project.  Exposed Lock object for SetProperty functionality.  Added action capability to basic SetProperty function.

--- UnitTestNemMvvm/Properties/AssemblyInfo.cs
+++ UnitTestNemMvvm/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NemMvvm")]
-[assembly: AssemblyDescription(".Net Library to make working with Mvvm Patterns easier.")]
+[assembly: AssemblyTitle("UnitTestNemMvvm")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Mark Moosman")]
-[assembly: AssemblyProduct("NemMvvm")]
-[assembly: AssemblyCopyright("Copyright ©  2016 Mark Moosman")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestNemMvvm")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ffb954b8-61ca-4ff2-b1e2-4ea2cfb2019c")]
+[assembly: Guid("d72a1cf8-6438-45c9-8616-ee5df5a0cc0d")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.1.1.*")]
-[assembly: AssemblyInformationalVersion("1.1.1-beta")]
-//[assembly: AssemblyVersion("1.1.*")]
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestNemMvvm/UnitTest1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NemMvvm;
+
+namespace UnitTestNemMvvm {
+	[TestClass]
+	public class UnitTest1 : NotifyPropertyChanged {
+		private string _testValue1 = "oldValue";
+		private string _testValue2 = "oldValue";
+		private string _testValue3 = "oldValue";
+		private bool? _actionCondition = null;
+
+		public string TestValue1 {
+			get {
+				return _testValue1;
+			}
+			set {
+				SetProperty(ref _testValue1, value);
+			}
+		}
+
+		public string TestValue2 {
+			get {
+				return _testValue2;
+			}
+			set {
+				_testValue3 = "notCompleted";
+
+				SetProperty(ref _testValue2, value, () => {
+					_testValue3 = "actionComplete";
+				}, _actionCondition);
+			}
+		}
+
+		[TestMethod]
+		public void SetPropertyTest() {
+			TestValue1 = "newValue";
+
+			Assert.AreEqual("newValue", TestValue1);
+		}
+
+		[TestMethod]
+		public void SetPropertyActionTest() {
+			_actionCondition = null;
+
+			TestValue2 = "newValue";
+
+			Assert.AreEqual("newValue", TestValue2);
+			Assert.AreEqual("actionComplete", _testValue3);
+		}
+
+		[TestMethod]
+		public void SetPropertyActionTestOnTrue() {
+			_actionCondition = true;
+
+			TestValue2 = "newValue";
+
+			Assert.AreEqual("newValue", TestValue2);
+			Assert.AreEqual("actionComplete", _testValue3);
+		}
+
+		[TestMethod]
+		public void SetPropertyActionTestOnFalse() {
+			_actionCondition = false;
+
+			_testValue2 = "newValue";
+			TestValue2 = "newValue";
+
+			Assert.AreEqual("newValue", TestValue2);
+			Assert.AreEqual("actionComplete", _testValue3);
+		}
+		[TestMethod]
+		public void SetPropertyActionTestNotCompleted() {
+			_actionCondition = false;
+
+			_testValue2 = "oldValue";
+			TestValue2 = "newValue";
+
+			Assert.AreEqual("newValue", TestValue2);
+			Assert.AreEqual("notCompleted", _testValue3);
+		}
+
+
+	}
+}
