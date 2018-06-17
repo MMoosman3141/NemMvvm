6e634d33d470f4ee408d39d7ce4d1038cc91990b
Mark Moosman <MMoosman3141@users.noreply.github.com>
2018-06-17 16:37:09 -0600

    Updated with Command objects will will take types to indicated parameters being passed to actions and functions.

--- NemMvvm/Command.cs
+++ NemMvvm/Command.cs
@@ -1,123 +1,71 @@
 ﻿using System;
-using System.Windows.Input;
 
 namespace NemMvvm {
   /// <summary>
   /// The Command class is a generic implementation of ICommand, allowing the specification of and Action to be executed, and a function to check if the action can be executed.
   /// </summary>
-  public class Command : ICommand {
-    private Action _execute;
-    private Func<bool> _canExecute;
-    private Action<object> _executeParamed;
-    private Func<object, bool> _canExecuteParamed;
-
+  public sealed class Command : BaseCommand<object, object> {
     /// <summary>
     /// Constructor of the Command object
     /// </summary>
     /// <param name="execute">The simple method called when the command object is invoked.</param>
-    public Command(Action execute) {
-      _execute = execute;
-      _canExecute = null;
-      _executeParamed = null;
-      _canExecuteParamed = null;
-    }
+    public Command(Action execute) : base(execute, null, null, null) { }
 
     /// <summary>
     /// Constructor of the Command object
     /// </summary>
     /// <param name="execute">The simple method called when the command object is invoked.</param>
     /// <param name="canExecute">The method which determines if the command can be executed.</param>
-    public Command(Action execute, Func<bool> canExecute) {
-      _execute = execute;
-      _canExecute = canExecute;
-      _executeParamed = null;
-      _canExecuteParamed = null;
-    }
+    public Command(Action execute, Func<bool> canExecute) : base(execute, canExecute, null, null) { }
 
     /// <summary>
     /// Constructor of the Command object
     /// </summary>
     /// <param name="execute">The simple method called when the command object is invoked.</param>
     /// <param name="canExecute">The method which determines if the command can be executed. Taking an object parameter.</param>
-    public Command(Action execute, Func<object, bool> canExecute) {
-      _execute = execute;
-      _canExecute = null;
-      _executeParamed = null;
-      _canExecuteParamed = canExecute;
-    }
+    [Obsolete("Use a typed Command rather than using a Command with object types.  This constructor will be removed in a future version.")]
+    public Command(Action execute, Func<object, bool> canExecute) : base(execute, null, null, canExecute) { }
 
     /// <summary>
     /// Constructor of the Command object
     /// </summary>
     /// <param name="execute">The method, taking an object parameter, to call when the command is invoked.</param>
-    public Command(Action<object> execute) {
-      _execute = null;
-      _canExecute = null;
-      _executeParamed = execute;
-      _canExecuteParamed = null;
-    }
+    [Obsolete("Use a typed Command rather than using a Command with object types.  This constructor will be removed in a future version.")]
+    public Command(Action<object> execute) : base(null, null, execute, null) { }
 
     /// <summary>
     /// Constructor of the Command object
     /// </summary>
     /// <param name="execute">The method, taking an object parameter, to call when the command is invoked.</param>
     /// <param name="canExecute">The method which determines if the command can be executed.</param>
-    public Command(Action<object> execute, Func<bool> canExecute) {
-      _execute = null;
-      _canExecute = canExecute;
-      _executeParamed = execute;
-      _canExecuteParamed = null;
-    }
+    [Obsolete("Use a typed Command rather than using a Command with object types.  This constructor will be removed in a future version.")]
+    public Command(Action<object> execute, Func<bool> canExecute) : base(null, canExecute ,execute, null) { }
 
     /// <summary>
     /// Constructor of the Command object
     /// </summary>
     /// <param name="execute">The method, taking an object parameter, to call when the command is invoked.</param>
     /// <param name="canExecute">The method which determines if the command can be executed. Taking an object parameter.</param>
-    public Command(Action<object> execute, Func<object, bool> canExecute) {
-      _execute = null;
-      _canExecute = null;
-      _executeParamed = execute;
-      _canExecuteParamed = canExecute;
+    [Obsolete("Use a typed Command rather than using a Command with object types.  This constructor will be removed in a future version.")]
+    public Command(Action<object> execute, Func<object, bool> canExecute) : base(null, null, execute, canExecute) { }
+
+    /// <summary>
+    /// Executes the Action specified in the constructor for execution
+    /// </summary>
+    public void Execute() {
+#pragma warning disable CS0618
+      base.Execute(null);
+#pragma warning restore CS0618
     }
 
     /// <summary>
-    /// Executes the command.  If the parameter is specified, and the Command object was constructed with an action accepting a parameter, the parameter is passed to the action.
+    /// Executes the Func specified in the constructor to see if execution is valid.
     /// </summary>
-    /// <param name="parameter">The parameter to pass to the action, if the Command object was constructed with an action taking a parameter.</param>
-    public void Execute(object parameter) {
-      if(_execute != null) {
-        _execute.Invoke();
-      } else if(_executeParamed != null) {
-        _executeParamed?.Invoke(parameter);
-      }
+    /// <returns></returns>
+    public bool CanExecute() {
+#pragma warning disable CS0618
+      return base.CanExecute(null);
+#pragma warning restore CS0618
     }
-
-    /// <summary>
-    /// Executes the CanExecute method, which determines if the Command object action can be invoked.  If the parameter is specified, and the Command object was constructed with a canExecute method which accepts a parameter, the parameter is passed to the canExecuteMethod.
-    /// </summary>
-    /// <param name="parameter">The parameter to pass to the action, if the Command object was constructed with a canExucute method wich accecpts a parameter.</param>
-    /// <returns>Return true or false based on the canExecute method established at construction time.</returns>
-    public bool CanExecute(object parameter) {
-      if(_canExecute != null) {
-        return _canExecute();
-      } else if(_canExecuteParamed != null) {
-        return _canExecuteParamed(parameter);
-      }
-
-      return true;
-    }
-
-    /// <summary>
-    /// Method to raise the CanExecuteChanged event which refreshes the CanExecute status of the command object.
-    /// </summary>
-    public void RaiseCanExecuteChanged() {
-      CanExecuteChanged?.Invoke(this, new EventArgs());
-    }
-
-    /// <summary>
-    /// Event called if the ability to execute the command object changes.
-    /// </summary>
-    public event EventHandler CanExecuteChanged;
-  }
-}
+  } //class
+} //namespace
---
+++ NemMvvm/INemCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace NemMvvm {
+  /// <summary>
+  /// Provides a common interface to expose the RaiseCanExecuteChanged method.
+  /// </summary>
+  public interface IFoundationCommand : ICommand {
+    /// <summary>
+    /// Raises the CanExecuteChanged event
+    /// </summary>
+    void RaiseCanExecuteChanged();
+  }
+}
